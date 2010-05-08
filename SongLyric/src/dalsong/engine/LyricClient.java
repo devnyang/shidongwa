fe7b79bfd6126d71a32108f1de97b3fe07972646
shidongwa <shidongwa@b8c64090-a89a-11de-a118-c7387b1e4f66>
2010-05-08 07:54:11 +0000

    get the lyric [mm:ss.ss] start point

--- SongLyric/src/dalsong/engine/LyricClient.java
+++ SongLyric/src/dalsong/engine/LyricClient.java
@@ -29,22 +29,8 @@ package dalsong.engine;
 
 
 
-import java.io.*;
-
-import java.net.*;
-import java.security.DigestInputStream;
-import java.security.MessageDigest;
-import java.util.ArrayList;
+import java.net.HttpURLConnection;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.farng.mp3.MP3File;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
 
 import dalsong.util.GAEUtil;
 import dalsong.util.SearchResult;
@@ -90,48 +76,6 @@ public class LyricClient {
         
         return content;
         
-	}
-
-	
-	private String getFileMD5_v2(String fileName)  throws Exception {
-		String s = "";
-	
-		MP3File file = new MP3File();
-		tagSize = (int)file.getMp3StartByte(new File(fileName));
-		
-		System.out.println("tag size : " + tagSize);
-		
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		FileInputStream fin = new FileInputStream(fileName);
-		
-		// 
-		DigestInputStream in = new DigestInputStream(fin,md);
-
-		fin.getChannel().position(tagSize);
-		byte[] b = new byte[163840];
-		in.read(b,0,163840);
-
-		md = in.getMessageDigest();
-		s = bytesToHex(md.digest());
-		
-		//
-//		System.out.println("MD5 Checksum : " + s);
-//		System.out.println("Tag size : " + tagSize);
-
-		fin.close();
-		in.close();
-		
-
-		return s;
-	}
-
-	private String bytesToHex(byte[] a) {
-		StringBuffer s = new StringBuffer();
-		for(int i=0;i<a.length;++i) {
-			s.append(Character.forDigit((a[i]>>4) & 0x0f, 16));
-			s.append(Character.forDigit(a[i] & 0x0f, 16));
-		}
-		return s.toString();            
 	}
 	
 	public int getTagSize(){
