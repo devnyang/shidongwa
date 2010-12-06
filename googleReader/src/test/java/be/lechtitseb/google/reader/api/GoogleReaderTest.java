ef08761c4d4277e999785d6954f2ff263d679564
shidongwa <shidongwa@b8c64090-a89a-11de-a118-c7387b1e4f66>
2010-12-06 12:11:57 +0000

    initial version, just for experiment, codes coming from internet .

---
+++ googleReader/src/test/java/be/lechtitseb/google/reader/api/GoogleReaderTest.java
@@ -0,0 +1,45 @@
+package be.lechtitseb.google.reader.api;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import be.lechtitseb.google.reader.api.core.GoogleReader;
+import be.lechtitseb.google.reader.api.model.exception.AuthenticationException;
+import be.lechtitseb.google.reader.api.model.exception.GoogleReaderException;
+import be.lechtitseb.google.reader.api.model.feed.ItemDescriptor;
+
+/**
+ * http://code.google.com/intl/ru-RU/apis/accounts/docs/AuthForInstalledApps.html#AuthProcess
+ * http://code.google.com/intl/ru-RU/apis/accounts/docs/OAuth.html
+ * @author gizatullinm
+ *
+ */
+public class GoogleReaderTest {
+
+	@Test
+	public void testAuth() throws FileNotFoundException, IOException, AuthenticationException, GoogleReaderException {
+		Properties auth = new Properties();
+		auth.load( new FileInputStream("auth.properties"));
+
+		GoogleReader googleReader = new GoogleReader(auth.getProperty("auth.key"),auth.getProperty("auth.secret"));
+		googleReader.login();
+		googleReader.getUserInformation();
+		List<ItemDescriptor> itemDesList = googleReader.getFeedItems("feed/http://echohfut.javaeye.com/rss");
+		//System.out.println(googleReader.getFeedItems("feed/http://echohfut.javaeye.com/rss"));
+		
+	}
+	
+	public static void main(String[] args) {
+		GoogleReaderTest test = new GoogleReaderTest();
+		try {
+			test.testAuth();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
